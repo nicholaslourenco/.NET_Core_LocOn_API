df1deac99dc27daf6f6198aae6397be86be78d4f
Nicholas Lourenco <nicklourenc@gmail.com>
2025-10-04 17:34:56 -0300

    Estrutura do Projeto

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocOn.Controllers
+{
+    public class FilmeController
+    {
+        
+    }
+}
